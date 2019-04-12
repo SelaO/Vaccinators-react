0183321a7f8bbb96e9293049931f89f8734c6e7f
SelaO <selaoren.a@gmail.com>
2019-04-12 08:27:06 +0300

    added login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import Drawer from "./components/Drawer";
+import Login from './Screens/Login'
 import "./App.css";
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {isLogin: true}
+
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    this.setState({isLogin: false})
+  }
+
   render() {
     return (
       <div className="App">
-        <Drawer />
+        {this.state.isLogin ? <Login onClick={() => this.login()}/> : <Drawer /> }
       </div>
     );
   }
